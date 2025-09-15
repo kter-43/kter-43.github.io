5ad4b6f3b2f2010df4a33efb18d2d0cd9c8d74cd
kter-43 <jatceng@gmail.com>
2025-09-15 14:40:17 +0100

    Update video.js

--- js/video.js
+++ js/video.js
@@ -9,18 +9,18 @@ fetch('video.json')
       table.innerHTML = `
         <tr>
           <td class="title-cell"><h4>${item.title}</h4></td>
-          <td class="audio-cell">        
+          <td class="composer-notes">${item.Composer}</td>
+        </tr>
+        <tr>
+          <td colspan="2" class="composer-notes">${item.Notes}</td>
+        </tr>
+        <tr>
+          <td colspan="2" class="audio-cell">        
             <video width="320" height="240" controls>
                 <source src="video/${item.mp4}" type="video/mp4">
                 Your browser does not support the video tag.
             </video>
           </td>
-        </tr>
-        <tr>
-          <td colspan="2" class="composer-notes">${item.Composer}</td>
-        </tr>
-        <tr>
-          <td colspan="2" class="composer-notes">${item.Notes}</td>
         </tr>
       `;
       container.appendChild(table);
