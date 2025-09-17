2bffd53e4d77f6239cc6d4f457070b4d2f286a25
kter-43 <jatceng@gmail.com>
2025-09-17 15:18:23 +0100

    Update links.js

--- js/links.js
+++ js/links.js
@@ -1,16 +1,21 @@
-
 fetch('links.json')
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('image-grid');
-    data.images.forEach(item => {
-      const link = document.createElement('a');
-      link.href = item.link;
-      const img = document.createElement('img');
-      img.src = item.src;
-      img.alt = item.alt || '';
-      link.appendChild(img);
-      container.appendChild(link);
-    });
-  })
-  .catch(error => console.error('Error loading images:', error));
+      .then(response => response.json())
+      .then(data => {
+        const container = document.getElementById('image-grid');
+        const contaiNav = document.getElementById('navigation');
+        data.images.forEach(item => {
+          const link = document.createElement('a');
+          link.href = item.link;
+          const img = document.createElement('img');
+          img.src = item.src;
+          img.alt = item.alt || '';
+          link.appendChild(img);
+          container.appendChild(link);
+          const nav = document.createElement('a');
+          nav.href = item.link;
+          nav.className = 'kter-bar-item kter-button kter-padding-large kter-hover-deep-purple kter-text-deep-purple';
+          nav.textContent = item.text;
+          contaiNav.appendChild(nav);
+        });
+      })
+      .catch(error => console.error('Error loading images:', error));
